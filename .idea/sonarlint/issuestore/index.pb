c91b78fbfd9a509952f6d9f9e73a46b5dd57fea6
mouhsinandroid <moushin.bourqaiba.partner@decathlon.com>
2024-07-15 18:44:58 -0400

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+m
+=app/src/main/java/com/mouhsinbourqaiba/runapp/MainActivity.kt,1\f\1f1c820088db56c6d44312aad02100ccd938e77a
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+C
+settings.gradle.kts,7\4\749edfcc96398253e5b3416184e95c46621da850
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RunApp"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(":app")
